028d50781a05eac03fbed2468ef9dea9e8d15b02
kimsomi0126 <kimsomi00126@gmail.com>
2024-03-27 10:56:30 +0900

    feat : Analytics 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,5 +11,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <RecoilRoot>
     <App />
+    <Analytics />
   </RecoilRoot>,
 );
